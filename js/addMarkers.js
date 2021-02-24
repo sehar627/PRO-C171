f8468e176c2baef53364dea87ac7760275492f74
PreetiSharma15 <pwhitehat6@gmail.com>
2021-02-24 18:27:49 +0530

    UI added for price(dollar)

--- js/addMarkers.js
+++ js/addMarkers.js
@@ -85,6 +85,34 @@ AFRAME.registerComponent("create-markers", {
           value: `${dish.ingredients.join("\n\n")}`
         });
         mainPlane.appendChild(ingredients);
+
+         //Plane to show the price of the dish
+         var pricePlane = document.createElement("a-image");
+         pricePlane.setAttribute("id", `price-plane-${dish.id}`);
+         pricePlane.setAttribute(
+           "src",
+           "https://raw.githubusercontent.com/whitehatjr/menu-card-app/main/black-circle.png"
+         );
+         pricePlane.setAttribute("width", 0.8);
+         pricePlane.setAttribute("height", 0.8);
+         pricePlane.setAttribute("position", { x: -1.3, y: 0, z: 0.3 });
+         pricePlane.setAttribute("rotation", { x: -90, y: 0, z: 0 });
+ 
+         //Price of the dish
+         var price = document.createElement("a-entity");
+         price.setAttribute("id", `price-${dish.id}`);
+         price.setAttribute("position", { x: 0.03, y: 0.05, z: 0.1 });
+         price.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+         price.setAttribute("text", {
+           font: "mozillavr",
+           color: "white",
+           width: 3,
+           align: "center",
+           value: `Only\n $${dish.price}`
+         });
+ 
+         pricePlane.appendChild(price);
+         marker.appendChild(pricePlane);
       }
     });
   },
